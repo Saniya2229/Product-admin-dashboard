0dde3229fb9aeae8d31cbe833d658e98c7f6f552
Saniya2229 <saniyahakim22@gmail.com>
2026-09-24 10:12:47 +0530

    feat: initial Next.js setup with Tailwind CSS, Axios, and Lucide React

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,22 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dummyjson.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
